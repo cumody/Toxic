10caa1bab97102b476c6c5e1b6f636e86b36cbbc
Mahmoud <cumody82@gmail.com>
2021-10-03 21:52:34 +0200

    Updating Content

--- app/src/main/java/com/mahmoudshaaban/toxic/ui/login/LoginContent.kt
+++ app/src/main/java/com/mahmoudshaaban/toxic/ui/login/LoginContent.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -25,45 +24,26 @@ import com.mahmoudshaaban.toxic.ui.components.ToxicTextField
 import com.mahmoudshaaban.toxic.ui.core.VerticalSpacer
 import com.mahmoudshaaban.toxic.ui.theme.ToxicTheme
 
+
 private const val APP_LOGO_WIDTH_PERCENTAGE = 0.75F
-
-
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.mahmoudshaaban.toxic.ui.components.PrimaryButton
-import com.mahmoudshaaban.toxic.ui.components.SecondaryButton
-import com.mahmoudshaaban.toxic.ui.theme.ToxicTheme
 
 /**
  * This[LoginContent] handles all configuration that happens for this screen
  * @param[viewState] the current state of the screen to render
  *
  */
+
 @Composable
 fun LoginContent(viewState: LoginViewState) {
 
     Surface(
-        color = MaterialTheme.colors.background
-fun LoginContent() {
-
-    Scaffold(
-        backgroundColor = MaterialTheme.colors.primary
     ) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(dimensionResource(id = R.dimen.screen_padding)),
             horizontalAlignment = Alignment.CenterHorizontally
-                .padding(24.dp)
+
         ) {
 
             Spacer(modifier = Modifier.weight(1F))
@@ -79,67 +59,31 @@ fun LoginContent() {
             ToxicTextField(
                 text = viewState.userName,
                 onTextChanged = {}, textLabel = stringResource(R.string.username)
-                )
-
-                VerticalSpacer(height = 12.dp)
-
-                ToxicTextField(
-                    text = viewState.password,
-                    onTextChanged = {}, textLabel = stringResource(R.string.password)
-                    )
-
-                    VerticalSpacer(height = 48.dp)
-
-                    PrimaryButton(
-                        text = stringResource(R.string.log_in),
-                        onclick = {},
-                    )
-                    VerticalSpacer(height = 12.dp)
-
-                    SecondaryButton(
-                        text = stringResource(R.string.sign_up),
-                        onclick = {},
-                    )
-                }
-            }
-        }
-
-        @Preview(
-            name = "Night Mode - Empty",
-            uiMode = Configuration.UI_MODE_NIGHT_YES
-        )
-        @Preview(
-            name = "Day Mode - Empty",
-            uiMode = Configuration.UI_MODE_NIGHT_NO
-        )
-        @Composable
-        @Suppress("UnusedPrivateMember")
-        private fun EmptyLoginScreenPreview() {
-
-            val viewState = LoginViewState(
-                userName = "",
-                password = ""
             )
 
-            ToxicTheme {
-                LoginContent(viewState)
-            }
-        }
+            VerticalSpacer(height = 12.dp)
+
+            ToxicTextField(
+                text = viewState.password,
+                onTextChanged = {}, textLabel = stringResource(R.string.password)
+            )
+
+            VerticalSpacer(height = 48.dp)
+
             PrimaryButton(
-                text = "Login In",
+                text = stringResource(R.string.log_in),
                 onclick = {},
-                backgroundColor = MaterialTheme.colors.secondary
             )
-            Spacer(modifier = Modifier.height(12.dp))
+            VerticalSpacer(height = 12.dp)
 
             SecondaryButton(
-                text = "Sign up",
+                text = stringResource(R.string.sign_up),
                 onclick = {},
-                contentColor = MaterialTheme.colors.onPrimary
             )
         }
     }
 }
+
 
 @Preview(
     name = "Night Mode - Empty",
@@ -159,6 +103,8 @@ private fun EmptyLoginScreenPreview() {
     )
 
     ToxicTheme {
-        LoginContent()
+        LoginContent(viewState)
     }
 }
+
+
