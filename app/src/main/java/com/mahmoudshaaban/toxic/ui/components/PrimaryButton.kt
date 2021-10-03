3eef8befc58ea72a8ec02f4ed70c6cc0f904c28e
Mahmoud <cumody82@gmail.com>
2021-10-03 16:19:36 +0200

    Pleasing the detekt.yml gods

--- app/src/main/java/com/mahmoudshaaban/toxic/ui/components/PrimaryButton.kt
+++ app/src/main/java/com/mahmoudshaaban/toxic/ui/components/PrimaryButton.kt
@@ -1,6 +1,5 @@
 package com.mahmoudshaaban.toxic.ui.components
 
-import android.content.res.Configuration
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.material.Button
@@ -13,21 +12,20 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.text.intl.Locale
 import androidx.compose.ui.text.toUpperCase
-import androidx.compose.ui.tooling.preview.Preview
 import com.mahmoudshaaban.toxic.R
 import com.mahmoudshaaban.toxic.ui.theme.ButtonShape
-import com.mahmoudshaaban.toxic.ui.theme.ToxicTheme
 
 @Composable
 fun PrimaryButton(
     modifier: Modifier = Modifier,
     text: String = "",
     onclick: () -> Unit = {},
-    color: Color = MaterialTheme.colors.primary
+    backgroundColor: Color = MaterialTheme.colors.primary,
+    textColor: Color = MaterialTheme.colors.onPrimary
 ) {
 
     val buttonColors = buttonColors(
-        backgroundColor = color
+        backgroundColor = backgroundColor
     )
 
     Button(
@@ -38,21 +36,9 @@ fun PrimaryButton(
             .height(dimensionResource(id = R.dimen.buttonHeight))
             .fillMaxWidth()
     ) {
-        Text(text = text.toUpperCase(Locale.current))
+        Text(text = text.toUpperCase(Locale.current), color = textColor)
     }
 }
 
-@Preview(
-    name = "Night Mode",
-    uiMode = Configuration.UI_MODE_NIGHT_YES
-)
-@Preview(
-    name = "Day Mode",
-    uiMode = Configuration.UI_MODE_NIGHT_NO
-)
-@Composable
-private fun PrimaryButtonPreview() {
-    ToxicTheme {
-        PrimaryButton(text = "Primary Button")
-    }
-}
+
+
