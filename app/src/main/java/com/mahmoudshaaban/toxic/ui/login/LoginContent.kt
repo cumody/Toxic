9af49c40d07a86790affb577587beb10f1600814
Mahmoud <cumody82@gmail.com>
2021-10-03 21:24:26 +0200

    Updating Content

--- app/src/main/java/com/mahmoudshaaban/toxic/ui/login/LoginContent.kt
+++ app/src/main/java/com/mahmoudshaaban/toxic/ui/login/LoginContent.kt
@@ -9,19 +9,23 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mahmoudshaaban.toxic.R
 import com.mahmoudshaaban.toxic.ui.components.PrimaryButton
 import com.mahmoudshaaban.toxic.ui.components.SecondaryButton
 import com.mahmoudshaaban.toxic.ui.components.ToxicTextField
+import com.mahmoudshaaban.toxic.ui.core.VerticalSpacer
 import com.mahmoudshaaban.toxic.ui.theme.ToxicTheme
+
+private const val APP_LOGO_WIDTH_PERCENTAGE = 0.75F
 
 /**
  * This[LoginContent] handles all configuration that happens for this screen
@@ -31,12 +35,13 @@ import com.mahmoudshaaban.toxic.ui.theme.ToxicTheme
 @Composable
 fun LoginContent(viewState: LoginViewState) {
 
-    Surface(color = MaterialTheme.colors.background
+    Surface(
+        color = MaterialTheme.colors.background
     ) {
         Column(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(24.dp),
+                .padding(dimensionResource(id = R.dimen.screen_padding)),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
 
@@ -44,60 +49,58 @@ fun LoginContent(viewState: LoginViewState) {
 
             Image(
                 painter = painterResource(id = R.drawable.ic_toxic_chekmark),
-                contentDescription = "App Logo",
-                modifier = Modifier.fillMaxWidth(0.75F)
+                contentDescription = stringResource(R.string.app_logo_content_description),
+                modifier = Modifier.fillMaxWidth(APP_LOGO_WIDTH_PERCENTAGE)
             )
 
             Spacer(modifier = Modifier.weight(1F))
 
-
             ToxicTextField(
                 text = viewState.userName,
-                onTextChanged = {}, textLabel = "Username"
-            )
+                onTextChanged = {}, textLabel = stringResource(R.string.username)
+                )
 
+                VerticalSpacer(height = 12.dp)
 
-            Spacer(modifier = Modifier.height(12.dp))
+                ToxicTextField(
+                    text = viewState.password,
+                    onTextChanged = {}, textLabel = stringResource(R.string.password)
+                    )
 
-            ToxicTextField(
-                text = viewState.password,
-                onTextChanged = {}, textLabel = "Password"
-            )
+                    VerticalSpacer(height = 48.dp)
 
-            Spacer(modifier = Modifier.height(48.dp))
+                    PrimaryButton(
+                        text = stringResource(R.string.log_in),
+                        onclick = {},
+                    )
+                    VerticalSpacer(height = 12.dp)
 
-            PrimaryButton(
-                text = "Login In",
-                onclick = {},
-            )
-            Spacer(modifier = Modifier.height(12.dp))
-
-            SecondaryButton(
-                text = "Sign up",
-                onclick = {},
-            )
+                    SecondaryButton(
+                        text = stringResource(R.string.sign_up),
+                        onclick = {},
+                    )
+                }
+            }
         }
-    }
-}
 
-@Preview(
-    name = "Night Mode - Empty",
-    uiMode = Configuration.UI_MODE_NIGHT_YES
-)
-@Preview(
-    name = "Day Mode - Empty",
-    uiMode = Configuration.UI_MODE_NIGHT_NO
-)
-@Composable
-@Suppress("UnusedPrivateMember")
-private fun EmptyLoginScreenPreview() {
+        @Preview(
+            name = "Night Mode - Empty",
+            uiMode = Configuration.UI_MODE_NIGHT_YES
+        )
+        @Preview(
+            name = "Day Mode - Empty",
+            uiMode = Configuration.UI_MODE_NIGHT_NO
+        )
+        @Composable
+        @Suppress("UnusedPrivateMember")
+        private fun EmptyLoginScreenPreview() {
 
-    val viewState = LoginViewState(
-        userName = "",
-        password = ""
-    )
+            val viewState = LoginViewState(
+                userName = "",
+                password = ""
+            )
 
-    ToxicTheme {
-        LoginContent(viewState)
-    }
-}
+            ToxicTheme {
+                LoginContent(viewState)
+            }
+        }
