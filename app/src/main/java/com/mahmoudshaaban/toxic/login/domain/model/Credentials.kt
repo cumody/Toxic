bad8a727e1057d705d7e6fc32e55c7c552ed3ff0
Mahmoud <cumody82@gmail.com>
2021-10-06 02:05:07 +0200

    Fixing issue related with Credentials.kt

--- app/src/main/java/com/mahmoudshaaban/toxic/login/domain/model/Credentials.kt
+++ app/src/main/java/com/mahmoudshaaban/toxic/login/domain/model/Credentials.kt
@@ -1,12 +1,12 @@
 package com.mahmoudshaaban.toxic.login.domain.model
 
 @JvmInline
+@Suppress("UnusedPrivateMember")
 value class Email(val value: String)
 
 @JvmInline
 @Suppress("UnusedPrivateMember")
 value class Password(val value: String)
-value class Password(val password: String)
 
 data class Credentials(
     val email: Email = Email(""),
